105e0e12ef8507e809ef2d61edb3c065e1574f06
TimLee <timleenights@gmail.com>
2017-06-28 02:51:41 +0800

    built a working web server

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 var http = require('http');
 var fs = require('fs');
+var extract = require('./extract.js');
 
-var server = http.createServer(function (req, res) {
+var handlerError = function (error, response) {
+  response.writeHead(404);
+  response.end('<h1>404, Not found !!!</h1>');
+}
+
+var server = http.createServer(function (request, response) {
   console.log('Responding to a request');
   // res.end('<h1>Hello, World!</h1>');
-  fs.readFile('app/index.html', function (err, data) {
-    res.end(data);
+
+  var filePath = extract(request.url);
+
+  fs.readFile(filePath, function (error, data) {
+    if (error) {
+      handlerError(error, response);
+      return;
+    } else {
+      response.end(data);
+    }
   });
 });
+
 server.listen(3000);
